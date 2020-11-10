1212ea05a63171434a428f853a58acd304d43950
jogannagari <48650279+jogannagari@users.noreply.github.com>
2020-11-10 13:49:15 +0530

    Add files via upload

---
+++ javas/index.js
@@ -0,0 +1,10 @@
+$(".angelaimage").mouseenter(function(){
+
+	var image=$(".angelaimage");
+
+	image.animate({opacity:'0.5'},'slow');
+	image.animate({	width:'400px'},'slow');
+	image.animate({opacity:'2'},'slow');
+	image.animate({	width:'150px'},'slow');
+
+});
